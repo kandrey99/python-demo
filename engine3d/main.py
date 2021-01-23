7e4c34e6557e2ec177ea80724c46a51009479af7
kandrey99 <kandrey@oviont.com>
2021-01-23 11:01:51 +0300

    add: matrix_functions

--- engine3d/main.py
+++ engine3d/main.py
@@ -1,40 +1,45 @@
-import pygame
-
-pygame.init()
-pygame.font.init()
-
-FPS = 60
+import pygame as pg
 
 
 class App:
     def __init__(self):
-        self.size = self.weight, self.height = 640, 400
+        pg.init()
+        self.fps = 60
+        self.size = self.width, self.height = 640, 400
+        self.h_width, self.h_height = self.width // 2, self.height // 2
+        self._display = pg.display.set_mode(self.size, pg.HWSURFACE | pg.DOUBLEBUF)
+        self._clock = pg.time.Clock()
         self._running = True
-        self._display = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._clock = pygame.time.Clock()
 
     def on_event(self, event):
-        if event.type == pygame.QUIT:
+        if event.type == pg.QUIT:
             self._running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
+        if event.type == pg.MOUSEBUTTONDOWN:
+            pos = pg.mouse.get_pos()
             # print(self._player._path._path)
 
     def update(self):
         pass
 
     def render(self):
-        pygame.display.flip()
+        self._display.fill(pg.Color('darkslategray'))
+        pg.display.flip()
 
     def cleanup(self):
-        pygame.quit()
+        pg.quit()
 
     def run(self):
         while self._running:
-            self._clock.tick(FPS)
-            for event in pygame.event.get():
+            self._clock.tick(self.fps)
+            pg.display.set_caption(str(self._clock.get_fps()))
+            for event in pg.event.get():
                 self.on_event(event)
             self.update()
             self.render()
         self.cleanup()
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
